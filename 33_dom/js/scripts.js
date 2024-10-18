16537c9b2fc37ad27056a97f0075c4941a282f11
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-10-17 21:50:59 -0300

    Acessando elementos através do DOM

---
+++ 33_dom/js/scripts.js
@@ -0,0 +1,15 @@
+// tag
+let titulo = document.getElementsByTagName('h1')[0];
+
+console.log("document.getElementsByTagName('h1')[0] :>> ", titulo);
+
+let  lis = document.getElementsByTagName('li');
+console.log("document.getElementsByTagName('li') :>> '", lis[3]);
+
+// id
+let paragrafo = document.getElementById('paragrafo');
+console.log("document.getElementById('paragrafo') :>> '", paragrafo);
+
+// class
+let itensDaLista = document.getElementsByClassName('item');
+console.log("document.getElementsByClassName('item') :>> ", itensDaLista);
