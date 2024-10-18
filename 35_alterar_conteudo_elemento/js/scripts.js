89977c7bd29b0ed3a8b658ecd489cda175c8cf48
José Queiroz <jose.queiroz@setling.pt>
2024-10-18 17:32:07 +0100

    Utilizando innerHTML e textContent

---
+++ 35_alterar_conteudo_elemento/js/scripts.js
@@ -0,0 +1,12 @@
+// selecionar elemento
+let title = document.querySelector("#title");
+console.log(title);
+
+// innerHTML
+title.innerHTML = "Teste o texto alterado";
+
+// textContent > mais utilizado, recomendando e performatico
+let subtitle = document.querySelector(".subtitle");
+console.log(subtitle);
+
+subtitle.textContent = "Testando o tectContet";
