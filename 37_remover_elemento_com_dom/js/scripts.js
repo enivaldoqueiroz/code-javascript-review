d325b70762f979ba19a195c5006b7de60d2b7e3d
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-10-19 09:55:58 -0300

    Remover elemento filho e elemento

---
+++ 37_remover_elemento_com_dom/js/scripts.js
@@ -0,0 +1,10 @@
+// removeChild() - Remover elemento filho
+let container = document.querySelector("#container");
+
+let p = document.querySelector("#container p");
+
+container.removeChild(p);
+
+// remove() - Remover o elemento
+let subtitle = document.querySelector(".subtitle");
+subtitle.remove();
