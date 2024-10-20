49e2edf2ca72e82dbbe1233ae4e7a9d7e9c37543
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-10-20 16:47:49 -0300

    ajuste

--- 41_alterar_css_no_dom/js/scripts.js
+++ 41_alterar_css_no_dom/js/scripts.js
@@ -1,12 +1,10 @@
-// Add CSS
-
-// 1º Seleciona o elemento
+// 1º Seleciona o elemento com id "title"
 let title = document.querySelector("#title");
 
-// 2º Adiciona o estilo
-title.style.color = "red";
-title.style.backgroundColor = "yellow";
+// 2º Adiciona os estilos ao elemento
+title.style.color = "red";  // Define a cor do texto como vermelho
+title.style.backgroundColor = "yellow";  // Define a cor de fundo como amarelo
 
-// Adicionar varios estilos
+// Adicionar vários estilos de uma só vez usando cssText
 let subtitle = document.querySelector(".subtitle");
 subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px";
