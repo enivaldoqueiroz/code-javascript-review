8fe420aac66f2b6245ec1e03679b9af01bb28b51
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-10-20 16:44:57 -0300

    Adicionando CSS pelo DOM

---
+++ 41_alterar_css_no_dom/js/scripts.js
@@ -0,0 +1,12 @@
+// Add CSS
+
+// 1º Seleciona o elemento
+let title = document.querySelector("#title");
+
+// 2º Adiciona o estilo
+title.style.color = "red";
+title.style.backgroundColor = "yellow";
+
+// Adicionar varios estilos
+let subtitle = document.querySelector(".subtitle");
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px";
