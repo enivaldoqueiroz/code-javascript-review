af2e3edb22f08c0d4e71a007b07c3c8ae7921bbb
José Queiroz <jose.queiroz@setling.pt>
2024-10-21 17:58:49 +0100

    Eventos Onload JS

---
+++ 46_eventos_onload/js/scripts.js
@@ -0,0 +1,12 @@
+// Evento Onload
+window.onload = function() {
+    console.log("Carregou o DOM");
+    let title = document.querySelector("#title");
+    console.log(title);
+}
+
+console.log("Carregou o JS");
+
+let title = document.querySelector("#title");
+
+console.log(title);
